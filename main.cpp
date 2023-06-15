12b1b9cec66e790e1e4f0a6bb03c88de676866ba
rchekr <rchekr@yandex.ru>
2023-06-15 15:33:20 +0300

    experiment 2

--- main.cpp
+++ main.cpp
@@ -2,107 +2,84 @@
 #include "SFML/Graphics.hpp"
 using namespace sf;
 
+struct pnt
+{
+    float x, y;
+    Color color;
+    CircleShape circ;
+    pnt() {}
+    pnt(float xx, float yy, Color col) : x(xx), y(yy), color(col)
+    {
+        circ.setPosition(Vector2f(x, y));
+        circ.setRadius(7);
+        circ.setOutlineColor(Color::Black);
+        circ.setOutlineThickness(2);
+        circ.setFillColor(col);
+    }
+};
+
 int main()
 {
+    const int height = 1280, width = 720, size = 20;
     ifstream infile("dataset.dat", ios::binary);
     vector<float> input(9), output(2), res(2);
-    vector<int> layers = { 9, 4, 3, 2 };
+    vector<int> layers = { 2, 5, 5, 2 };
     Network network(layers, 1);
-    network.load();
-    float er = 0;
-    bool b;
-    int p = 0, n = 0;
-    do
-    {   
-        system("cls");
-        er = 0; p = 0;
-        for (int j = 0; j < 16; j++)
+    RenderWindow window(VideoMode(height, width), "Neural network", Style::Close);
+    vector<pnt> points(0);
+    vector<vector<RectangleShape>> screen(height / size);
+    for (int i = 0; i < width / size; i++)
+    {
+        screen[i] = vector<RectangleShape>(height / size, RectangleShape(Vector2f(size, size)));
+        for (int j = 0; j < height / size; j++)
         {
-            for (int i = 0; i < 9; i++)
-            {
-                infile.seekg(p++);
-                infile.read((char*)&b, sizeof(bool));
-                input[i] = b;
-            }
-            for (int i = 0; i < 2; i++)
-            {
-                infile.read((char*)&b, sizeof(bool));
-                output[i] = b ? 0.8 : 0.2;
-                infile.seekg(++p);
-            }
-            res = network.signal_passing(input);
-            er += network.back_propogation(output);
-            for (int i = 0; i < 9; i++) cout << input[i] << " ";
-            for (int i = 0; i < 2; i++) cout << output[i] << " ";
-            cout << res[0] << " " << res[1] << endl;
+            screen[i][j].setFillColor(Color(0, 255, 255));
+            screen[i][j].setPosition(Vector2f(j * size, i * size));
         }
-        cout << er << " " << n++ << endl << endl;
-    } while (er > 1e-6 && n < 30000);
-    network.store();
-    RenderWindow window(VideoMode(1920, 1080), "Neural network", Style::Close);
-    vector<RectangleShape> inputs(9, RectangleShape(Vector2f(200, 200)));
-    vector<RectangleShape> outputs(2, RectangleShape(Vector2f(200, 200)));
-    Font font;
-    font.loadFromFile("arialmt.ttf");
-    Text text;
-    Text H, V, horout, verout;
-    H.setString("hor"); H.setFont(font);  H.setCharacterSize(120); H.setPosition(Vector2f(1230, 220));
-    V.setString("ver"); V.setFont(font); V.setCharacterSize(120); V.setPosition(Vector2f(1230, 425));
-    horout.setFont(font);  horout.setCharacterSize(80); horout.setPosition(Vector2f(1500, 250));
-    verout.setFont(font);  verout.setCharacterSize(80); verout.setPosition(Vector2f(1500, 455));
-    float x, y;
-    for (int i = 0; i < 9; i++) 
-    {
-    inputs[i].setFillColor(Color::Black);
-    inputs[i].setOutlineColor(Color::White);
-    inputs[i].setOutlineThickness(4);
-    inputs[i].setPosition(Vector2f(200 + 215 * (i % 3), 200 + 215 * (i / 3)));
-    if (i < 2)
-    {
-        outputs[i].setFillColor(Color::Black);
-        outputs[i].setOutlineColor(Color::White);
-        outputs[i].setOutlineThickness(4);
-        outputs[i].setPosition(Vector2f(1000, 200 + 215 * (i % 3)));
-    }
     }
     while (window.isOpen())
     {
-    sf::Event event;
-    while (window.pollEvent(event))
-    {
-        switch (event.type)
+        sf::Event event;
+        while (window.pollEvent(event))
         {
-        case Event::Closed: window.close(); break;
-        case Event::MouseButtonPressed:
-            x = event.mouseButton.x;
-            y = event.mouseButton.y;
-            for (int i = 0; i < 9; i++)
-                if (x > inputs[i].getPosition().x && x < inputs[i].getPosition().x + 208 &&
-                    y > inputs[i].getPosition().y && y < inputs[i].getPosition().y + 208)
-                    inputs[i].setFillColor((inputs[i].getFillColor() == Color::White) ? Color::Black : Color::White);
+            switch (event.type)
+            {
+            case Event::Closed: window.close(); break;
+            case Event::MouseButtonPressed:
+                if (event.mouseButton.button == Mouse::Left)
+                    points.push_back(pnt(event.mouseButton.x, event.mouseButton.y, Color::Green));
+                else points.push_back(pnt(event.mouseButton.x, event.mouseButton.y, Color::Blue));
+                break;
+            default: 
+                for (int i = 0; i < points.size(); i++)
+                {
+                    float x = points[i].x / (0.5 * height) - 1;
+                    float y = points[i].y / (0.5 * width) - 1;
+                    res = network.signal_passing(vector<float>{x, y});
+                    cout << res[0] << " " << res[1] << endl;
+                    network.back_propogation(vector<float>{(float)points[i].color.g / 255, (float)points[i].color.b / 255});
+                }
+                if (points.size())
+                {
+                    for (int i = 0; i < width / size; i++)
+                        for (int j = 0; j < height / size; j++)
+                        {
+                            Vector2f vec = screen[i][j].getPosition();
+                            float x = (vec.x) / (0.5 * height) - 1;
+                            float y = (vec.y) / (0.5 * width) - 1;
+                            res = network.signal_passing(vector<float>{x, y});
+                            screen[i][j].setFillColor(Color(50, res[0] * 255, res[1] * 255));
+                        }
+                }
             break;
-        default: 
-        for (int i = 0; i < 9; i++)
-        {
-            input[i] = (inputs[i].getFillColor() == Color::White) ? 1 : 0;
+            }
         }
-        res = network.signal_passing(input);
-        horout.setString(to_string(res[0]));
-        outputs[0].setFillColor((res[0] >= 0.7 && res[0] <= 0.9) ? Color::White : Color::Black);
-        outputs[1].setFillColor((res[1] >= 0.7 && res[1] <= 0.9) ? Color::White : Color::Black);
-        verout.setString(to_string(res[1]));
-        break;
-        }
-    }
-    window.clear();
-    for (int i = 0; i < 9; i++) window.draw(inputs[i]);
-    for (int i = 0; i < 2; i++) window.draw(outputs[i]); 
-    window.draw(horout);
-    window.draw(verout);
-    window.draw(text);
-    window.draw(H);
-    window.draw(V);
-    window.display();
+        for (int i = 0; i < width / size; i++)
+            for (int j = 0; j < height / size; j++)
+                window.draw(screen[i][j]);
+        for (int i = 0; i < points.size(); i++)
+            window.draw(points[i].circ);
+        window.display();
     } 
 	return 0;
 }
